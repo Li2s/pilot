c20654a27c176c69971f43ed2a4f0239edf67c9e
Nikita Lituev <lituev.nikita@gmail.com>
2021-07-09 16:43:40 +0300

    Comments and reqs are added

--- Drivers/Sensor_Proc/SensorProc.c
+++ Drivers/Sensor_Proc/SensorProc.c
@@ -1,11 +1,52 @@
+/**********************************************************************/
+/*(C) Copyright 2021                                                  */
+/* File: SensorProc.c                                                 */
+/* Library for analog sensor processing                               */
+/*--------------------------------------------------------------------*/
+/* Change history                                                     */
+/* Num   |   Date   |     Author     |    PR     |    Description     */ 
+/*  0    |24.06.2021|  Lituev N.A.   |    NA     |  Initial commit    */
+/*  1    |09.07.2021|  Lituev N.A.   |    NA     |  Comments added    */
+/*--------------------------------------------------------------------*/
+
+
+/*--------------------------------------------------------------------*/
+/*Includes                                                            */
+/*--------------------------------------------------------------------*/
 #include "SensorProc.h"
 #include "math.h"
+/*--------------------------------------------------------------------*/
 
+
+/*--------------------------------------------------------------------*/
+/*Functions                                                           */
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+/*Function: getAdcRaw                                                 */
+/*Description: returns the raw ADC conversion result                  */
+/*Requirements: BRUD_SDD_SW_REQ_1                                     */
+/*Parameters:                                                         */
+/*    hadc  (input) - pointer to the ADC hardware handler             */
+/*Return value:                                                       */
+/*     uint16_t - the raw result of the ADC conversion                */
+/*--------------------------------------------------------------------*/
 uint16_t getAdcRaw(ADC_HandleTypeDef* hadc)
 {
 	return HAL_ADC_GetValue(hadc);
 }
 
+
+
+/*--------------------------------------------------------------------*/
+/*Function: checkValueAdc                                             */
+/*Description: checks that the conversion result is correct           */
+/*Requirements: BRUD_SDD_SW_REQ_6                                     */
+/*Parameters:                                                         */
+/*    l_adc_raw  (input) - result of the ADC conversion               */
+/*Return value:                                                       */
+/*     uint8_t - 1 if the value is correct, 0 if it is not correct    */
+/*--------------------------------------------------------------------*/
 uint8_t checkValueAdc(uint16_t l_adc_raw)
 {
 	const uint16_t QUAN_LEVELS = 4096; 
@@ -22,6 +63,16 @@ uint8_t checkValueAdc(uint16_t l_adc_raw)
 }
 
 
+
+/*--------------------------------------------------------------------*/
+/*Function: getVoltageAdc                                             */
+/*Description: converts the ADC result into the voltage               */
+/*Requirements: BRUD_SDD_SW_REQ_2, BRUD_SDD_SW_REQ_3                  */
+/*Parameters:                                                         */
+/*    l_adc_raw  (input) - result of the ADC conversion               */
+/*Return value:                                                       */
+/*     float - voltage of the sensor                                  */
+/*--------------------------------------------------------------------*/
 float getVoltageAdc(uint16_t l_adc_raw)
 {
 	const uint16_t QUAN_LEVELS = 4096;
@@ -31,6 +82,16 @@ float getVoltageAdc(uint16_t l_adc_raw)
 }
 
 
+
+/*--------------------------------------------------------------------*/
+/*Function: getRotationAngle                                          */
+/*Description: converts the voltage into the angle value              */
+/*Requirements: BRUD_SDD_SW_REQ_4, BRUD_SDD_SW_REQ_5                  */
+/*Parameters:                                                         */
+/*    l_voltage_adc  (input) - voltage from the sensor                */
+/*Return value:                                                       */
+/*     float - rotattion angle of the sensor                          */
+/*--------------------------------------------------------------------*/
 float getRotationAngle(float l_voltage_adc)
 {
 	const float MAX_ANGLE = 90.0;
@@ -38,3 +99,4 @@ float getRotationAngle(float l_voltage_adc)
 	
 	return (MAX_ANGLE/MAX_VOLTAGE)*l_voltage_adc;
 }
+/*--------------------------------------------------------------------*/
